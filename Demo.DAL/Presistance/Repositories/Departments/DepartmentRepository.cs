0fe692f63cddec8853dacb95fe7b7199af51efd5
you22ef <102329215+you22ef@users.noreply.github.com>
2025-04-27 06:25:14 +0300

    Add DepartmentRepository and IDepartmentRepository
    Introduced `DepartmentRepository` class and `IDepartmentRepository`
    interface for managing `Departments` table operations. Implemented
    methods for CRUD operations, including `GetAll`, `GetById`,
    `AddDepartment`, `UpdateDepartment`, and `DeleteDepartment`.
    Utilized dependency injection with `ApplicationDbContext` and
    added comments to clarify repository role in architecture.

---
+++ Demo.DAL/Presistance/Repositories/Departments/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using Demo.DAL.Entities.Departments;
+using Demo.DAL.Presistance.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Presistance.Repositories.Departments
+{
+    //Database <== Repository <== Service <== Controller
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext) // Dependency Injection
+        {
+            _dbContext = dbContext;   
+        }   
+        public IEnumerable<Department> GetAll(bool AsNoTracking = true)
+        {
+            if (AsNoTracking)
+            {
+                return _dbContext.Departments.AsNoTracking().ToList();
+            }
+
+            return _dbContext.Departments.ToList();
+        }
+
+        public Department? GetById(int id)
+        {
+            //return _dbContext.Departments.Local.FirstOrDefault(d => d.Id == id);
+            return _dbContext.Departments.Find(id);//search locally in case found ==> retrun else{send request to database}
+        }
+        public int AddDepartment(Department department)
+        {
+            _dbContext.Departments.Add(department);
+            return _dbContext.SaveChanges();
+        }
+
+        public int UpdateDepartment(Department department)
+        {
+            _dbContext.Departments.Update(department);
+            return _dbContext.SaveChanges();
+        }
+
+        public int DeleteDepartment(Department department)
+        {
+            _dbContext.Departments.Remove(department);
+            return _dbContext.SaveChanges();
+        }
+
+    }
+}
---
+++ Demo.DAL/Presistance/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo.DAL.Entities.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Presistance.Repositories.Departments
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool AsNoTracking = true);
+        Department? GetById(int id);
+        int AddDepartment(Department department);
+        int UpdateDepartment(Department department);
+        int DeleteDepartment(Department department);
+    }
+}
